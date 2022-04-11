e0913af26ed5710245abb0bf03f31e52fb177dbf
piyushmittal578 <79057263+piyushmittal578@users.noreply.github.com>
2022-04-12 01:34:10 +0530

    Update forecast.js

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,7 +1,7 @@
 const key = "aIAGZdaDnvakTf92ifNgXFoJ14tPEtl8";
 
 const getWeather = async (locationkey) => {
-  const base = `http://dataservice.accuweather.com/currentconditions/v1/${locationkey}`;
+  const base = `https://dataservice.accuweather.com/currentconditions/v1/${locationkey}`;
   const query = `?apikey=${key}`;
 
   const response = await fetch(base + query);
@@ -10,7 +10,7 @@ const getWeather = async (locationkey) => {
 };
 
 const getCity = async (city) => {
-  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const base = "https://dataservice.accuweather.com/locations/v1/cities/search";
   const query = `?apikey=${key}&q=${city}`;
 
   const response = await fetch(base+query);
